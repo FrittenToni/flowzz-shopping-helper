d8a1f837ea914d924adcba3c795cec3d0cd14301
frittentoni <fritten@toni.com>
2024-06-18 15:28:18 +0200

    allow extension to be opened no matter if user is on flowzz or not

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,45 @@
 <script lang="ts">
-	import { onMount } from "svelte";
 	import SyncStrains from "$components/SyncStrains.svelte";
 	import SearchStrains from "$components/SearchStrains.svelte";
-
+  
 	let cannabisStrains = [];
 	let strainCount = 0;
-	let showError = false;
-
+  
 	function handleStrainsUpdated(event) {
-		cannabisStrains = event.detail.cannabisStrains;
-		strainCount = event.detail.strainCount;
+	  cannabisStrains = event.detail.cannabisStrains;
+	  strainCount = event.detail.strainCount;
 	}
-
+  
 	function handleResetView() {
-		cannabisStrains = [];
-		strainCount = 0;
+	  cannabisStrains = [];
+	  strainCount = 0;
 	}
-
-	function navigateToFlowzz() {
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.update(tabs[0].id, { url: "https://flowzz.com/" });
-		});
-	}
-
-	onMount(() => {
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			const currentTab = tabs[0];
-			if (!currentTab.url.startsWith("https://flowzz.com/")) {
-				showError = true;
-			}
-		});
-	});
-</script>
-
-{#if showError}
-	<div class="error">
-		<p>
-			Open 
-			<a href="#" on:click|preventDefault={navigateToFlowzz}>flowzz.com</a> 
-			and log on to use this extension.
-		</p>
+  </script>
+  
+  <main>
+	<div class="header">
+	  <h1>Flowzz Shopping Helper</h1>
 	</div>
-{:else}
-	<main>
-		<div class="header">
-			<h1>Flowzz Shopping Helper</h1>
-		</div>
-
-		<SyncStrains
-			on:strainsUpdated={handleStrainsUpdated}
-			on:resetView={handleResetView}
-		/>
-
-		{#if strainCount > 0}
-			<SearchStrains {cannabisStrains} {strainCount} />
-		{/if}
-	</main>
-{/if}
-
-<style>
+  
+	<SyncStrains
+	  on:strainsUpdated={handleStrainsUpdated}
+	  on:resetView={handleResetView}
+	/>
+  
+	{#if strainCount > 0}
+	  <SearchStrains {cannabisStrains} {strainCount} />
+	{/if}
+  </main>
+  
+  <style>
 	main {
-		text-align: center;
+	  text-align: center;
 	}
 	.header {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		color: green;
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	  color: green;
 	}
-	.error {
-		background-color: green;
-		color: white;
-		text-align: center;
-		padding: 2rem;
-		font-size: 1.5rem;
-	}
-	.error a {
-		color: white;
-		text-decoration: underline;
-		cursor: pointer;
-	}
-</style>
+  </style>
+  
