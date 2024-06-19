8ccd3e411ac0183a247548d96daa1f22811b1f7b
frittentoni <fritten@toni.com>
2024-06-19 09:38:59 +0200

    split advanced serach into filtergroup. fix headline in strain table

--- src/components/search/FilterGroup.svelte
+++ src/components/search/FilterGroup.svelte
@@ -1,43 +1,112 @@
 <script lang="ts">
-    export let priceMin;
-    export let priceMax;
-    export let thcMin;
-    export let thcMax;
-    export let ratingMin;
-    export let scoreCountMin;
-    export let sortOption;
-    export let sortOrder;
-    export let showAvailableOnly;
-    export let searchStrain;
-    export let cannabisStrains;
+    import { createEventDispatcher, onMount } from 'svelte';
   
-    export let saveState;
-    export let clearFilters;
-    export let loadState;
+    export let cannabisStrains = [];
+  
+    let priceMin = 0;
+    let priceMax = '';
+    let thcMin = 0;
+    let thcMax = 100;
+    let ratingMin = 0;
+    let scoreCountMin = 0;
+    let sortOption = '';
+    let sortOrder = 'asc';
+    let showAvailableOnly = false;
+    let searchStrain = '';
+  
+    const dispatch = createEventDispatcher();
+  
+    function saveState() {
+      const state = {
+        priceMin,
+        priceMax,
+        thcMin,
+        thcMax,
+        ratingMin,
+        scoreCountMin,
+        sortOption,
+        sortOrder,
+        showAvailableOnly,
+        searchStrain
+      };
+      localStorage.setItem('advancedSearchState', JSON.stringify(state));
+    }
+  
+    function loadState() {
+      const state = JSON.parse(localStorage.getItem('advancedSearchState'));
+      if (state) {
+        priceMin = state.priceMin;
+        priceMax = state.priceMax;
+        thcMin = state.thcMin;
+        thcMax = state.thcMax;
+        ratingMin = state.ratingMin;
+        scoreCountMin = state.scoreCountMin;
+        sortOption = state.sortOption;
+        sortOrder = state.sortOrder;
+        showAvailableOnly = state.showAvailableOnly;
+        searchStrain = state.searchStrain;
+      }
+    }
+  
+    function clearFilters() {
+      priceMin = 0;
+      priceMax = '';
+      thcMin = 0;
+      thcMax = 100;
+      ratingMin = 0;
+      scoreCountMin = 0;
+      sortOption = '';
+      sortOrder = 'asc';
+      showAvailableOnly = false;
+      searchStrain = '';
+      saveState();
+      handleFilterChange();
+    }
+  
+    function handleFilterChange() {
+      saveState();
+      dispatch('filterChange', {
+        priceMin,
+        priceMax,
+        thcMin,
+        thcMax,
+        ratingMin,
+        scoreCountMin,
+        sortOption,
+        sortOrder,
+        showAvailableOnly,
+        searchStrain
+      });
+    }
+  
+    onMount(() => {
+      loadState();
+      handleFilterChange();
+    });
   </script>
   
   <div class="filter-group">
     <div class="filter-item">
       <label>Price Range:</label>
-      <input type="number" bind:value={priceMin} min="0" placeholder="Min Price" on:input={saveState}/>
-      <input type="number" bind:value={priceMax} min="0" placeholder="Max Price" on:input={saveState}/>
+      <input type="number" bind:value={priceMin} min="0" placeholder="Min Price" on:input={handleFilterChange}/>
+      <input type="number" bind:value={priceMax} min="0" placeholder="Max Price" on:input={handleFilterChange}/>
     </div>
     <div class="filter-item">
       <label>THC Range:</label>
-      <input type="number" bind:value={thcMin} min="0" max="100" placeholder="Min THC" on:input={saveState}/>
-      <input type="number" bind:value={thcMax} min="0" max="100" placeholder="Max THC" on:input={saveState}/>
+      <input type="number" bind:value={thcMin} min="0" max="100" placeholder="Min THC" on:input={handleFilterChange}/>
+      <input type="number" bind:value={thcMax} min="0" max="100" placeholder="Max THC" on:input={handleFilterChange}/>
     </div>
     <div class="filter-item">
       <label>Review Score:</label>
-      <input type="number" bind:value={ratingMin} min="0" max="5" step="0.1" placeholder="Min Score" on:input={saveState}/>
+      <input type="number" bind:value={ratingMin} min="0" max="5" step="0.1" placeholder="Min Score" on:input={handleFilterChange}/>
     </div>
     <div class="filter-item">
       <label>Min Reviews:</label>
-      <input type="number" bind:value={scoreCountMin} min="0" placeholder="Min Score Count" on:input={saveState}/>
+      <input type="number" bind:value={scoreCountMin} min="0" placeholder="Min Score Count" on:input={handleFilterChange}/>
     </div>
     <div class="filter-item">
       <label>Sort By:</label>
-      <select bind:value={sortOption} on:change={saveState}>
+      <select bind:value={sortOption} on:change={handleFilterChange}>
         <option value="">None</option>
         <option value="name">Name</option>
         <option value="price">Price</option>
@@ -45,7 +114,7 @@
         <option value="thc">THC</option>
         <option value="reviewCount">Review Count</option>
       </select>
-      <select bind:value={sortOrder} on:change={saveState}>
+      <select bind:value={sortOrder} on:change={handleFilterChange}>
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
@@ -56,7 +125,7 @@
         list="strainNames"
         bind:value={searchStrain}
         placeholder="Enter strain name"
-        on:input={saveState}
+        on:input={handleFilterChange}
       />
       <datalist id="strainNames">
         {#each cannabisStrains as strain}
@@ -66,7 +135,7 @@
     </div>
     <div class="filter-item checkbox-item">
       <label class="checkbox-container">
-        <input type="checkbox" bind:checked={showAvailableOnly} on:change={saveState}/>
+        <input type="checkbox" bind:checked={showAvailableOnly} on:change={handleFilterChange}/>
         Show Available Only
       </label>
     </div>
@@ -109,10 +178,13 @@
       width: 150px;
     }
     button {
-      margin-top: 0px;
+      margin-top: 1rem;
       padding: 0.5rem 1rem;
       font-size: 1rem;
       cursor: pointer;
+    }
+    .clear-button {
+      margin-top: 0px;
     }
   </style>
   
--- src/components/search/StrainTable.svelte
+++ src/components/search/StrainTable.svelte
@@ -1,122 +1,123 @@
 <script lang="ts">
     import RatingSection from './RatingSection.svelte';
-  
+
     export let filteredStrains;
-  
+
     let activeRatings = {};
-  
+
     function formatStrainName(strain_name) {
-      return strain_name
-        .replace(/\s+/g, '-') // Replace whitespace with dash
-        .replace(/[^\w-]+/g, '') // Remove all other special characters
-        .toLowerCase();
+        return strain_name
+            .replace(/\s+/g, '-') // Replace whitespace with dash
+            .replace(/[^\w-]+/g, '') // Remove all other special characters
+            .toLowerCase();
     }
-  
+
     function openInCurrentTab(url) {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.update(tabs[0].id, { url: url });
-      });
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.update(tabs[0].id, { url: url });
+        });
     }
-  
+
     function handleShowRatings(id) {
-      if (activeRatings[id]) {
-        delete activeRatings[id];
-      } else {
-        activeRatings = { ...activeRatings, [id]: 'loading' };
-        fetchRatings(id);
-      }
-    }
-  
-    async function fetchRatings(id) {
-      let ratings = [];
-      let start = 0;
-      let hasMore = true;
-  
-      while (hasMore) {
-        const response = await fetch(`https://flowzz.com/api/raitings/${id}?t=1&id=${id}&start=${start}`);
-        const data = await response.json();
-  
-        if (data.message.data.ratings.length > 0) {
-          ratings = [...ratings, ...data.message.data.ratings];
-          start += 20;
+        if (activeRatings[id]) {
+            delete activeRatings[id];
         } else {
-          hasMore = false;
+            activeRatings = { ...activeRatings, [id]: 'loading' };
+            fetchRatings(id);
         }
-      }
-  
-      activeRatings = { ...activeRatings, [id]: ratings.length > 0 ? ratings : 'No ratings found.' };
     }
-  
+
+    async function fetchRatings(id) {
+        let ratings = [];
+        let start = 0;
+        let hasMore = true;
+
+        while (hasMore) {
+            const response = await fetch(`https://flowzz.com/api/raitings/${id}?t=1&id=${id}&start=${start}`);
+            const data = await response.json();
+
+            if (data.message.data.ratings.length > 0) {
+                ratings = [...ratings, ...data.message.data.ratings];
+                start += 20;
+            } else {
+                hasMore = false;
+            }
+        }
+
+        activeRatings = { ...activeRatings, [id]: ratings.length > 0 ? ratings : 'No ratings found.' };
+    }
+
     function handleCloseRatings(id) {
-      delete activeRatings[id];
-      activeRatings = { ...activeRatings };
+        delete activeRatings[id];
+        activeRatings = { ...activeRatings };
     }
-  </script>
-  
-  <table>
+</script>
+
+<h3>Filtered Strains ({filteredStrains.length})</h3>
+
+<table>
     <thead>
-      <tr>
-        <th>Name</th>
-        <th>Details</th>
-        <th>THC</th>
-        <th>Rating</th>
-        <th>Price Range</th>
-      </tr>
+        <tr>
+            <th>Name</th>
+            <th>Details</th>
+            <th>THC</th>
+            <th>Rating</th>
+            <th>Price Range</th>
+        </tr>
     </thead>
     <tbody>
-      {#each filteredStrains as strain}
-        <tr>
-          <td>
-            <a class="clickable" href={"https://flowzz.com/product/" + strain.url} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://flowzz.com/product/" + strain.url); }}>
-              {strain.name}
-            </a>
-            <a class="clickable" href={"https://flowzz.com/strain/" + formatStrainName(strain.strain_name)} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://flowzz.com/strain/" + formatStrainName(strain.strain_name)); }}>
-              ({formatStrainName(strain.strain_name)})
-            </a>
-          </td>
-          <td>
-            <a class="clickable" href={"https://www.leafly.com/search?q=" + strain.strain_name} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://www.leafly.com/search?q=" + strain.strain_name); }}>Leafly</a> |
-            <a class="clickable" href={"https://www.cannaconnection.com/search?controller=search&orderby=position&orderway=desc&search_query=" + strain.strain_name} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://www.cannaconnection.com/search?controller=search&orderby=position&orderway=desc&search_query=" + strain.strain_name); }}>Cannaconnection</a>
-          </td>
-          <td>{strain.thc}%</td>
-          <td>
-            {strain.ratings_score ?? 0}
-            <a class="clickable" on:click={(e) => { e.preventDefault(); handleShowRatings(strain.id); }}>
-              ({strain.ratings_count ?? 0} reviews)
-            </a>
-          </td>
-          <td>{strain.min_price} - {strain.max_price}</td>
-        </tr>
-        {#if activeRatings[strain.id]}
-          <tr>
-            <td colspan="5">
-              <RatingSection strainId={strain.id} ratings={activeRatings[strain.id]} on:close={() => handleCloseRatings(strain.id)} />
-            </td>
-          </tr>
-        {/if}
-      {/each}
+        {#each filteredStrains as strain}
+            <tr>
+                <td>
+                    <a class="clickable" href={"https://flowzz.com/product/" + strain.url} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://flowzz.com/product/" + strain.url); }}>
+                        {strain.name}
+                    </a>
+                    <a class="clickable" href={"https://flowzz.com/strain/" + formatStrainName(strain.strain_name)} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://flowzz.com/strain/" + formatStrainName(strain.strain_name)); }}>
+                        ({formatStrainName(strain.strain_name)})
+                    </a>
+                </td>
+                <td>
+                    <a class="clickable" href={"https://www.leafly.com/search?q=" + strain.strain_name} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://www.leafly.com/search?q=" + strain.strain_name); }}>Leafly</a> |
+                    <a class="clickable" href={"https://www.cannaconnection.com/search?controller=search&orderby=position&orderway=desc&search_query=" + strain.strain_name} on:click={(e) => { e.preventDefault(); openInCurrentTab("https://www.cannaconnection.com/search?controller=search&orderby=position&orderway=desc&search_query=" + strain.strain_name); }}>Cannaconnection</a>
+                </td>
+                <td>{strain.thc}%</td>
+                <td>
+                    {strain.ratings_score ?? 0}
+                    <a class="clickable" on:click={(e) => { e.preventDefault(); handleShowRatings(strain.id); }}>
+                        ({strain.ratings_count ?? 0} reviews)
+                    </a>
+                </td>
+                <td>{strain.min_price} - {strain.max_price}</td>
+            </tr>
+            {#if activeRatings[strain.id]}
+                <tr>
+                    <td colspan="5">
+                        <RatingSection strainId={strain.id} ratings={activeRatings[strain.id]} on:close={() => handleCloseRatings(strain.id)} />
+                    </td>
+                </tr>
+            {/if}
+        {/each}
     </tbody>
-  </table>
-  
-  <style>
+</table>
+
+<style>
     table {
-      width: 100%;
-      border-collapse: collapse;
-      margin-top: 1rem;
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 1rem;
     }
     th, td {
-      border: 1px solid #ddd;
-      padding: 8px;
-      text-align: left;
-      color: black;
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: left;
+        color: black;
     }
     th {
-      background-color: #f2f2f2;
+        background-color: #f2f2f2;
     }
     .clickable {
-      cursor: pointer;
-      color: blue;
-      text-decoration: underline;
+        cursor: pointer;
+        color: blue;
+        text-decoration: underline;
     }
-  </style>
-  
+</style>
