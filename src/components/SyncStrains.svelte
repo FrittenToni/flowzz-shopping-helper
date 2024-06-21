0dcd5e87630160390e51872e5f71c5a91803f289
frittentoni <fritten@toni.com>
2024-06-22 00:31:21 +0200

    auto detect if flowzz has new strains and sync them

--- src/components/SyncStrains.svelte
+++ src/components/SyncStrains.svelte
@@ -3,165 +3,162 @@
 	import { fetchStrains } from "$lib/syncStrains.ts";
 	import AdvancedSearch from "$components/search/AdvancedSearch.svelte";
 	import SearchStrains from "$components/pricefinder/SearchStrains.svelte";
-
+  
 	let loading = false;
 	let message = "";
 	let strainCount = 0;
 	let cannabisStrains = [];
 	let showAdvancedSearch = false;
 	let showPriceFinder = false;
-
+  
 	const dispatch = createEventDispatcher();
-
+  
 	function updateMessage(newMessage) {
-		message = newMessage;
+	  message = newMessage;
 	}
-
+  
 	async function loadStrains() {
-		const storedStrains = localStorage.getItem("cannabisStrains");
-		if (storedStrains) {
-			const strains = JSON.parse(storedStrains);
-			cannabisStrains = strains.map((strain) => ({
-				id: strain.id,
-				name: strain.name,
-				thc: strain.thc,
-				cbd: strain.cbd,
-				genetic: strain.genetic,
-				ratings_score: strain.ratings_score,
-				ratings_count: strain.ratings_count,
-				min_price: strain.min_price,
-				max_price: strain.max_price,
-				url: strain.url,
-				availibility: strain.availibility,
-				strain_name: strain.strain_name,
-			}));
-			strainCount = strains.length;
-			updateMessage(`${strainCount} Cannabis Strains available`);
-			dispatch("strainsUpdated", { cannabisStrains, strainCount });
-		} else {
-			await fetchAndStoreStrains();
+	  const storedStrains = localStorage.getItem("cannabisStrains");
+	  if (storedStrains) {
+		const strains = JSON.parse(storedStrains);
+		cannabisStrains = strains.map((strain) => ({
+		  id: strain.id,
+		  name: strain.name,
+		  thc: strain.thc,
+		  cbd: strain.cbd,
+		  genetic: strain.genetic,
+		  ratings_score: strain.ratings_score,
+		  ratings_count: strain.ratings_count,
+		  min_price: strain.min_price,
+		  max_price: strain.max_price,
+		  url: strain.url,
+		  availibility: strain.availibility,
+		  strain_name: strain.strain_name,
+		}));
+		strainCount = strains.length;
+		updateMessage(`${strainCount} Cannabis Strains available`);
+		dispatch("strainsUpdated", { cannabisStrains, strainCount });
+		
+		// Fetch total number of products and compare
+		const totalProducts = await fetchTotalProducts();
+		if (totalProducts && totalProducts !== strainCount) {
+		  await fetchAndStoreStrains();
 		}
-	}
-
-	async function fetchAndStoreStrains() {
-		loading = true;
-		await fetchStrains(updateMessage);
-		const storedStrains = localStorage.getItem("cannabisStrains");
-		if (storedStrains) {
-			const strains = JSON.parse(storedStrains);
-			cannabisStrains = strains.map((strain) => ({
-				id: strain.id,
-				name: strain.name,
-				thc: strain.thc,
-				cbd: strain.cbd,
-				genetic: strain.genetic,
-				ratings_score: strain.ratings_score,
-				ratings_count: strain.ratings_count,
-				min_price: strain.min_price,
-				max_price: strain.max_price,
-				url: strain.url,
-				availibility: strain.availibility,
-				strain_name: strain.strain_name,
-			}));
-			strainCount = strains.length;
-			dispatch("strainsUpdated", { cannabisStrains, strainCount });
-		}
-		loading = false;
-	}
-
-	async function refreshStrains() {
-		dispatch("resetView");
+	  } else {
 		await fetchAndStoreStrains();
+	  }
 	}
-
+  
+	async function fetchTotalProducts() {
+	  try {
+		const response = await fetch("https://flowzz.com/api/products");
+		const data = await response.json();
+		return data.message.meta.pagination.total;
+	  } catch (error) {
+		console.error("Failed to fetch total products:", error);
+		return null;
+	  }
+	}
+  
+	async function fetchAndStoreStrains() {
+	  loading = true;
+	  await fetchStrains(updateMessage);
+	  const storedStrains = localStorage.getItem("cannabisStrains");
+	  if (storedStrains) {
+		const strains = JSON.parse(storedStrains);
+		cannabisStrains = strains.map((strain) => ({
+		  id: strain.id,
+		  name: strain.name,
+		  thc: strain.thc,
+		  cbd: strain.cbd,
+		  genetic: strain.genetic,
+		  ratings_score: strain.ratings_score,
+		  ratings_count: strain.ratings_count,
+		  min_price: strain.min_price,
+		  max_price: strain.max_price,
+		  url: strain.url,
+		  availibility: strain.availibility,
+		  strain_name: strain.strain_name,
+		}));
+		strainCount = strains.length;
+		dispatch("strainsUpdated", { cannabisStrains, strainCount });
+	  }
+	  loading = false;
+	}
+  
 	function toggleAdvancedSearch() {
-		showAdvancedSearch = !showAdvancedSearch;
-		saveState();
+	  showAdvancedSearch = !showAdvancedSearch;
+	  saveState();
 	}
-
+  
 	function togglePriceFinder() {
-		showPriceFinder = !showPriceFinder;
-		saveState();
+	  showPriceFinder = !showPriceFinder;
+	  saveState();
 	}
-
+  
 	function saveState() {
-		const state = {
-			showAdvancedSearch,
-			showPriceFinder
-		};
-		localStorage.setItem("viewState", JSON.stringify(state));
+	  const state = {
+		showAdvancedSearch,
+		showPriceFinder
+	  };
+	  localStorage.setItem("viewState", JSON.stringify(state));
 	}
-
+  
 	function loadState() {
-		const storedViewState = localStorage.getItem("viewState");
-		if (storedViewState !== null) {
-			const state = JSON.parse(storedViewState);
-			showAdvancedSearch = state.showAdvancedSearch;
-			showPriceFinder = state.showPriceFinder;
-		}
+	  const storedViewState = localStorage.getItem("viewState");
+	  if (storedViewState !== null) {
+		const state = JSON.parse(storedViewState);
+		showAdvancedSearch = state.showAdvancedSearch;
+		showPriceFinder = state.showPriceFinder;
+	  }
 	}
-
+  
 	onMount(() => {
-		loadStrains();
-		loadState();
+	  loadStrains();
+	  loadState();
 	});
-</script>
-
-<main>
+  </script>
+  
+  <main>
 	{#if loading}
-		<p>{message}</p>
+	  <p>{message}</p>
 	{:else}
-		<div>
-			{#if strainCount > 0}
-			<img
-				src="/refresh.png"
-				alt="Refresh Strains"
-				class="refresh-icon"
-				on:click={refreshStrains}
-			/>
-			{/if}
-			<div>
-				{message}
-			</div>
-			
-			{#if strainCount > 0}
-				<br/>
-				<div>
-					<button on:click={toggleAdvancedSearch}>
-						{showAdvancedSearch
-							? "Close Advanced Search"
-							: "Open Advanced Search"}
-					</button>
-					<button on:click={togglePriceFinder}>
-						{showPriceFinder
-							? "Close Price Finder"
-							: "Open Price Finder"}
-					</button>
-				</div>
-			{/if}
-		</div>
-		{#if showAdvancedSearch}
-			<AdvancedSearch {cannabisStrains} />
+	  <div>
+		<div>{message}</div>
+		{#if strainCount > 0}
+		  <br />
+		  <div>
+			<button on:click={toggleAdvancedSearch}>
+			  {showAdvancedSearch
+				? "Close Advanced Search"
+				: "Open Advanced Search"}
+			</button>
+			<button on:click={togglePriceFinder}>
+			  {showPriceFinder
+				? "Close Price Finder"
+				: "Open Price Finder"}
+			</button>
+		  </div>
 		{/if}
-		{#if showPriceFinder}
-			<SearchStrains {cannabisStrains} {strainCount} />
-		{/if}
+	  </div>
+	  {#if showAdvancedSearch}
+		<AdvancedSearch {cannabisStrains} />
+	  {/if}
+	  {#if showPriceFinder}
+		<SearchStrains {cannabisStrains} {strainCount} />
+	  {/if}
 	{/if}
-</main>
-
-<style>
+  </main>
+  
+  <style>
 	main {
-		text-align: center;
-		padding: 1rem;
-		width: 600px;
-		color: green;
-	}
-	.refresh-icon {
-		width: 14px;
-		height: 14px;
-		cursor: pointer;
+	  text-align: center;
+	  padding: 1rem;
+	  width: 600px;
+	  color: green;
 	}
 	button {
-		margin: 0.5rem;
+	  margin: 0.5rem;
 	}
-</style>
+  </style>
+  
