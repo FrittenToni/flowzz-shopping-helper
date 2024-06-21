3eacaeef46240118ac55a6e72e299648c7866d8e
frittentoni <fritten@toni.com>
2024-06-22 00:10:49 +0200

    add cultivar search

--- src/components/search/AdvancedSearch.svelte
+++ src/components/search/AdvancedSearch.svelte
@@ -1,57 +1,83 @@
 <script lang="ts">
-  import FilterGroup from './FilterGroup.svelte';
+  import { onMount } from 'svelte';
   import StrainTable from './StrainTable.svelte';
+  import FilterGroup from './FilterGroup.svelte';
 
   export let cannabisStrains = [];
 
   let filteredStrains = [];
+  let priceMin = 0;
+  let priceMax = '';
+  let thcMin = 0;
+  let thcMax = 100;
+  let ratingMin = 0;
+  let scoreCountMin = 0;
+  let sortOption = '';
+  let sortOrder = 'asc';
+  let showAvailableOnly = false;
+  let searchStrain = '';
+  let searchCultivar = '';
 
   function handleFilterChange(event) {
-    const {
-      priceMin,
-      priceMax,
-      thcMin,
-      thcMax,
-      ratingMin,
-      scoreCountMin,
-      sortOption,
-      sortOrder,
-      showAvailableOnly,
-      searchStrain
-    } = event.detail;
+      const {
+          priceMin,
+          priceMax,
+          thcMin,
+          thcMax,
+          ratingMin,
+          scoreCountMin,
+          sortOption,
+          sortOrder,
+          showAvailableOnly,
+          searchStrain,
+          searchCultivar
+      } = event.detail;
 
-    filteredStrains = cannabisStrains.filter(strain => {
-      const isPriceInRange = priceMax === '' ? strain.min_price >= priceMin : (strain.min_price >= priceMin && strain.min_price <= priceMax);
-      const isThcInRange = strain.thc >= thcMin && strain.thc <= thcMax;
-      const isRatingInRange = (strain.ratings_score ?? 0) >= ratingMin && (strain.ratings_count ?? 0) >= scoreCountMin;
-      const isAvailable = showAvailableOnly ? strain.availibility === 1 : true;
-      const isStrainMatch = searchStrain === '' ? true : strain.name.toLowerCase().includes(searchStrain.toLowerCase());
+      filteredStrains = cannabisStrains.filter(strain => {
+          const isPriceInRange = priceMax === '' ? strain.min_price >= priceMin : (strain.min_price >= priceMin && strain.min_price <= priceMax);
+          const isThcInRange = strain.thc >= thcMin && strain.thc <= thcMax;
+          const isRatingInRange = (strain.ratings_score ?? 0) >= ratingMin && (strain.ratings_count ?? 0) >= scoreCountMin;
+          const isAvailable = showAvailableOnly ? strain.availibility === 1 : true;
+          const isStrainMatch = searchStrain === '' ? true : strain.name.toLowerCase().includes(searchStrain.toLowerCase());
+          const isCultivarMatch = searchCultivar === '' ? true : formatStrainName(strain.strain_name).includes(searchCultivar.toLowerCase());
 
-      return isPriceInRange && isThcInRange && isRatingInRange && isAvailable && isStrainMatch;
-    }).sort((a, b) => {
-      let comparison = 0;
-      switch (sortOption) {
-        case 'name':
-          comparison = a.name.localeCompare(b.name);
-          break;
-        case 'price':
-          comparison = a.min_price - b.min_price;
-          break;
-        case 'rating':
-          comparison = b.ratings_score - a.ratings_score;
-          break;
-        case 'thc':
-          comparison = b.thc - a.thc;
-          break;
-        case 'reviewCount':
-          comparison = b.ratings_count - a.ratings_count;
-          break;
-        default:
-          break;
-      }
-      return sortOrder === 'asc' ? comparison : -comparison;
-    });
+          return isPriceInRange && isThcInRange && isRatingInRange && isAvailable && isStrainMatch && isCultivarMatch;
+      }).sort((a, b) => {
+          let comparison = 0;
+          switch (sortOption) {
+              case 'name':
+                  comparison = a.name.localeCompare(b.name);
+                  break;
+              case 'price':
+                  comparison = a.min_price - b.min_price;
+                  break;
+              case 'rating':
+                  comparison = b.ratings_score - a.ratings_score;
+                  break;
+              case 'thc':
+                  comparison = b.thc - a.thc;
+                  break;
+              case 'reviewCount':
+                  comparison = b.ratings_count - a.ratings_count;
+                  break;
+              default:
+                  break;
+          }
+          return sortOrder === 'asc' ? comparison : -comparison;
+      });
   }
+
+  function formatStrainName(strain_name) {
+      return strain_name
+          .trim()
+          .replace(/\s+/g, '-') // Replace whitespace with dash
+          .replace(/[^\w-]+/g, '') // Remove all other special characters
+          .toLowerCase();
+  }
+
+  onMount(() => {
+      handleFilterChange({ detail: { priceMin, priceMax, thcMin, thcMax, ratingMin, scoreCountMin, sortOption, sortOrder, showAvailableOnly, searchStrain, searchCultivar } });
+  });
 </script>
 
 <main>
@@ -62,7 +88,7 @@
 
 <style>
   main {
-    text-align: center;
-    padding: 1rem;
+      text-align: center;
+      padding: 1rem;
   }
 </style>
--- src/components/search/FilterGroup.svelte
+++ src/components/search/FilterGroup.svelte
@@ -1,54 +1,57 @@
 <script lang="ts">
-    import { createEventDispatcher, onMount } from 'svelte';
-  
-    export let cannabisStrains = [];
-  
-    let priceMin = 0;
-    let priceMax = '';
-    let thcMin = 0;
-    let thcMax = 100;
-    let ratingMin = 0;
-    let scoreCountMin = 0;
-    let sortOption = '';
-    let sortOrder = 'asc';
-    let showAvailableOnly = false;
-    let searchStrain = '';
-  
-    const dispatch = createEventDispatcher();
-  
-    function saveState() {
+  import { createEventDispatcher, onMount } from 'svelte';
+
+  export let cannabisStrains = [];
+
+  let priceMin = 0;
+  let priceMax = '';
+  let thcMin = 0;
+  let thcMax = 100;
+  let ratingMin = 0;
+  let scoreCountMin = 0;
+  let sortOption = '';
+  let sortOrder = 'asc';
+  let showAvailableOnly = false;
+  let searchStrain = '';
+  let searchCultivar = '';
+
+  const dispatch = createEventDispatcher();
+
+  function saveState() {
       const state = {
-        priceMin,
-        priceMax,
-        thcMin,
-        thcMax,
-        ratingMin,
-        scoreCountMin,
-        sortOption,
-        sortOrder,
-        showAvailableOnly,
-        searchStrain
+          priceMin,
+          priceMax,
+          thcMin,
+          thcMax,
+          ratingMin,
+          scoreCountMin,
+          sortOption,
+          sortOrder,
+          showAvailableOnly,
+          searchStrain,
+          searchCultivar
       };
       localStorage.setItem('advancedSearchState', JSON.stringify(state));
-    }
-  
-    function loadState() {
+  }
+
+  function loadState() {
       const state = JSON.parse(localStorage.getItem('advancedSearchState'));
       if (state) {
-        priceMin = state.priceMin;
-        priceMax = state.priceMax;
-        thcMin = state.thcMin;
-        thcMax = state.thcMax;
-        ratingMin = state.ratingMin;
-        scoreCountMin = state.scoreCountMin;
-        sortOption = state.sortOption;
-        sortOrder = state.sortOrder;
-        showAvailableOnly = state.showAvailableOnly;
-        searchStrain = state.searchStrain;
+          priceMin = state.priceMin;
+          priceMax = state.priceMax;
+          thcMin = state.thcMin;
+          thcMax = state.thcMax;
+          ratingMin = state.ratingMin;
+          scoreCountMin = state.scoreCountMin;
+          sortOption = state.sortOption;
+          sortOrder = state.sortOrder;
+          showAvailableOnly = state.showAvailableOnly;
+          searchStrain = state.searchStrain;
+          searchCultivar = state.searchCultivar;
       }
-    }
-  
-    function clearFilters() {
+  }
+
+  function clearFilters() {
       priceMin = 0;
       priceMax = '';
       thcMin = 0;
@@ -59,132 +62,157 @@
       sortOrder = 'asc';
       showAvailableOnly = false;
       searchStrain = '';
+      searchCultivar = '';
       saveState();
       handleFilterChange();
-    }
-  
-    function handleFilterChange() {
+  }
+
+  function handleFilterChange() {
       saveState();
       dispatch('filterChange', {
-        priceMin,
-        priceMax,
-        thcMin,
-        thcMax,
-        ratingMin,
-        scoreCountMin,
-        sortOption,
-        sortOrder,
-        showAvailableOnly,
-        searchStrain
+          priceMin,
+          priceMax,
+          thcMin,
+          thcMax,
+          ratingMin,
+          scoreCountMin,
+          sortOption,
+          sortOrder,
+          showAvailableOnly,
+          searchStrain,
+          searchCultivar
       });
-    }
-  
-    onMount(() => {
+  }
+
+  function formatStrainName(strain_name) {
+      return strain_name
+          .trim()
+          .replace(/\s+/g, '-') // Replace whitespace with dash
+          .replace(/[^\w-]+/g, '') // Remove all other special characters
+          .toLowerCase();
+  }
+
+  const uniqueCultivars = [...new Set(cannabisStrains.map(strain => formatStrainName(strain.strain_name)))];
+
+  onMount(() => {
       loadState();
       handleFilterChange();
-    });
-  </script>
-  
-  <div class="filter-group">
-    <div class="filter-item">
+  });
+</script>
+
+<div class="filter-group">
+  <div class="filter-item">
       <label>Price Range:</label>
       <input type="number" bind:value={priceMin} min="0" placeholder="Min Price" on:input={handleFilterChange}/>
       <input type="number" bind:value={priceMax} min="0" placeholder="Max Price" on:input={handleFilterChange}/>
-    </div>
-    <div class="filter-item">
+  </div>
+  <div class="filter-item">
       <label>THC Range:</label>
       <input type="number" bind:value={thcMin} min="0" max="100" placeholder="Min THC" on:input={handleFilterChange}/>
       <input type="number" bind:value={thcMax} min="0" max="100" placeholder="Max THC" on:input={handleFilterChange}/>
-    </div>
-    <div class="filter-item">
+  </div>
+  <div class="filter-item">
       <label>Review Score:</label>
       <input type="number" bind:value={ratingMin} min="0" max="5" step="0.1" placeholder="Min Score" on:input={handleFilterChange}/>
-    </div>
-    <div class="filter-item">
+  </div>
+  <div class="filter-item">
       <label>Min Reviews:</label>
       <input type="number" bind:value={scoreCountMin} min="0" placeholder="Min Score Count" on:input={handleFilterChange}/>
-    </div>
-    <div class="filter-item">
+  </div>
+  <div class="filter-item">
       <label>Sort By:</label>
       <select bind:value={sortOption} on:change={handleFilterChange}>
-        <option value="">None</option>
-        <option value="name">Name</option>
-        <option value="price">Price</option>
-        <option value="rating">Rating</option>
-        <option value="thc">THC</option>
-        <option value="reviewCount">Review Count</option>
+          <option value="">None</option>
+          <option value="name">Name</option>
+          <option value="price">Price</option>
+          <option value="rating">Rating</option>
+          <option value="thc">THC</option>
+          <option value="reviewCount">Review Count</option>
       </select>
       <select bind:value={sortOrder} on:change={handleFilterChange}>
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
       </select>
-    </div>
-    <div class="filter-item">
+  </div>
+  <div class="filter-item">
       <label>Name:</label>
       <input
-        list="strainNames"
-        bind:value={searchStrain}
-        placeholder="Enter strain name"
-        on:input={handleFilterChange}
+          list="strainNames"
+          bind:value={searchStrain}
+          placeholder="Enter strain name"
+          on:input={handleFilterChange}
       />
       <datalist id="strainNames">
-        {#each cannabisStrains as strain}
-          <option value={strain.name}>{strain.name}</option>
-        {/each}
+          {#each cannabisStrains as strain}
+              <option value={strain.name}>{strain.name}</option>
+          {/each}
       </datalist>
-    </div>
-    <div class="filter-item checkbox-item">
-      <label class="checkbox-container">
-        <input type="checkbox" bind:checked={showAvailableOnly} on:change={handleFilterChange}/>
-        Show Available Only
-      </label>
-    </div>
-    <div class="filter-item">
-      <button class="clear-button" on:click={clearFilters}>Clear</button>
-    </div>
   </div>
-  
-  <style>
-    .filter-group {
+  <div class="filter-item">
+      <label>Cultivar:</label>
+      <input
+          list="cultivarNames"
+          bind:value={searchCultivar}
+          placeholder="Enter cultivar"
+          on:input={handleFilterChange}
+      />
+      <datalist id="cultivarNames">
+          {#each uniqueCultivars as cultivar}
+              <option value={cultivar}>{cultivar}</option>
+          {/each}
+      </datalist>
+  </div>
+  <div class="filter-item checkbox-item">
+      <label class="checkbox-container">
+          <input type="checkbox" bind:checked={showAvailableOnly} on:change={handleFilterChange}/>
+          Show Available Only
+      </label>
+  </div>
+  <div class="filter-item">
+      <button class="clear-button" on:click={clearFilters}>Clear</button>
+  </div>
+</div>
+
+<style>
+  .filter-group {
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
       gap: 1rem;
       margin-bottom: 1rem;
-    }
-    .filter-item {
+  }
+  .filter-item {
       display: flex;
       flex-direction: column;
       align-items: center;
-    }
-    .checkbox-item {
+  }
+  .checkbox-item {
       display: flex;
       align-items: center;
-    }
-    .checkbox-container {
+  }
+  .checkbox-container {
       display: flex;
       align-items: center;
-    }
-    .checkbox-container input[type="checkbox"] {
+  }
+  .checkbox-container input[type="checkbox"] {
       width: auto;
       margin-right: 0.5rem;
-    }
-    label {
+  }
+  label {
       margin-bottom: 0.5rem;
-    }
-    input, select {
+  }
+  input, select {
       padding: 0.5rem;
       font-size: 1rem;
       width: 150px;
-    }
-    button {
+  }
+  button {
       margin-top: 1rem;
       padding: 0.5rem 1rem;
       font-size: 1rem;
       cursor: pointer;
-    }
-    .clear-button {
+  }
+  .clear-button {
       margin-top: 0px;
-    }
-  </style>
-  
+  }
+</style>
