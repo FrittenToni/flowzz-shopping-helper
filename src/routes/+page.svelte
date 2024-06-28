8c328da2dd3d8cfd924e8fbaefcbfce6f23c0cdf
frittentoni <fritten@toni.com>
2024-06-28 17:49:52 +0200

    use new logo also in extension itself

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,38 @@
 <script lang="ts">
 	import SyncStrains from "$components/SyncStrains.svelte";
-  
+	
 	let cannabisStrains = [];
 	let strainCount = 0;
-  
+	
 	function handleStrainsUpdated(event) {
 	  cannabisStrains = event.detail.cannabisStrains;
 	  strainCount = event.detail.strainCount;
 	}
-  
+	
 	function handleResetView() {
 	  cannabisStrains = [];
 	  strainCount = 0;
 	}
-</script>
-  
-<main>
-	<div class="header">
-		<h1>Flowzz Shopping Helper</h1>
-	</div>
-  
+  </script>
+	
+  <main>
+	<img src="/flowzz-shopping-helper.png" alt="Extension Icon" class="extension-icon">
+
 	<SyncStrains
-		on:strainsUpdated={handleStrainsUpdated}
-		on:resetView={handleResetView}
-		{cannabisStrains}
-		{strainCount}
+	  on:strainsUpdated={handleStrainsUpdated}
+	  on:resetView={handleResetView}
+	  {cannabisStrains}
+	  {strainCount}
 	/>
-</main>
-  
-<style>
+  </main>
+	
+  <style>
 	main {
-		text-align: center;
+	  text-align: center;
 	}
-	.header {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		color: green;
+	.extension-icon {
+	  width: 200px;
+	  height: 200px;
 	}
-</style>
+  </style>
+  
